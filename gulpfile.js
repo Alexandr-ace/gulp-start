1ea6b15d83bf522d51fa735da64f47266db1046a
Alexandr-ace <syakubin@bk.ru>
2021-09-28 21:28:49 +0300

    scripts

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const { src, dest, watch, parallel } = require("gulp");
 const scss = require("gulp-sass")(require("sass"));
 const concat = require("gulp-concat");
 const browserSync = require("browser-sync").create();
+const uglify = require("gulp-uglify-es").default;
 
 function browsersynk() {
     browserSync.init({
@@ -9,6 +10,14 @@ function browsersynk() {
             baseDir: "./app/",
         },
     });
+}
+
+function scripts() {
+    return src(["./app/js/app.js", "./app/js/main.js"])
+        .pipe(concat("main.min.js"))
+        .pipe(uglify())
+        .pipe(dest("app/js"))
+        .pipe(browserSync.stream());
 }
 
 function styles() {
@@ -21,11 +30,12 @@ function styles() {
 
 function watching() {
     watch(["app/scss/**/*.scss"], styles);
+    watch(["app/js/**/*.js", "!app/js/main.min.js"], scripts);
     watch(["app/*.html"]).on("change", browserSync.reload);
 }
 
 exports.styles = styles;
 exports.watching = watching;
 exports.browsersynk = browsersynk;
-exports.default = styles;
-exports.default = parallel(browsersynk, watching);
+exports.scripts = scripts;
+exports.default = parallel(scripts, browsersynk, watching);
